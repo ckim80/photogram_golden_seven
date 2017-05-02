7e44c67452209a4a6be85dd9a4f940c178b5d62e
Chong Kim <ckim80@chicagobooth.edu>
2017-05-01 22:31:29 -0500

    Progress 2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,14 +7,15 @@ class PhotosController < ApplicationController
   end
 
   def create_row
-    url = params[:the_source]
-    cap = params[:the_caption]
+    url = params[:The_source]
+    cap = params[:The_caption]
     new_photo = Photo.new
     new_photo.source = url
     new_photo.caption = cap
     new_photo.save
     # render("photos/create_row.html.erb")
-    redirect_to("/photos/#{new_photo.id}")
+    redirect_to("/photos")
+    #{new_photo.id}"
   end
 
   def new_form
@@ -25,23 +26,31 @@ class PhotosController < ApplicationController
     the_id = params[:id]
     @my_photo = Photo.find(the_id)
     render("photos/show.html.erb")
-
   end
 
   def edit_form
-    id = params[:id]
-    @my_photo = Photo.find(id)
-
-
+    the_id = params[:id]
+    @my_photo = Photo.find(the_id)
     render("photos/edit_form.html.erb")
   end
 
   def update_row
-    render("photos/update_row.html.erb")
+    url_edit = params[:source]
+    cap_edit = params[:caption]
+
+    photo_edit = Photo.find(params[:id])
+    photo_edit.source = url_edit
+    photo_edit.caption = cap_edit
+    photo_edit.save
+    redirect_to("/photos/:id")
+    # render("photos/update_row.html.erb")
   end
 
   def destroy_row
-    render("photos/destroy_row.html.erb")
+    photo = Photo.find(params[:id])
+    photo.destroy
+    # render("photos/destroy_row.html.erb")
+    redirect_to("/photos")
   end
 
 end
